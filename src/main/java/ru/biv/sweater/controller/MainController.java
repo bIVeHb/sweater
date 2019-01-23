bb2aa96ecb710b7f36648417b238e6170e4e733c
Igor Babanov <forbiv@yahoo.com>
2019-01-23 11:39:09 +0300

    Add author field for Message (OneToMany mapping)

--- src/main/java/ru/biv/sweater/controller/MainController.java
+++ src/main/java/ru/biv/sweater/controller/MainController.java
@@ -1,11 +1,13 @@
 package ru.biv.sweater.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.biv.sweater.domain.Message;
+import ru.biv.sweater.domain.User;
 import ru.biv.sweater.repository.MessageRepository;
 
 import java.util.Map;
@@ -29,8 +31,11 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(@AuthenticationPrincipal User user,
+                      @RequestParam String text,
+                      @RequestParam String tag, Map<String,
+                      Object> model) {
+        Message message = new Message(text, tag, user);
         repository.save(message);
         Iterable<Message> messages = repository.findAll();
         model.put("messages", messages);
